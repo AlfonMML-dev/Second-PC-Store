2465230492cc31147d389cde29e099a5e91e14e3
AlfonMML-dev <amartinmoreno251@ieszaidinvergeles.org>
2021-12-14 07:43:40 +0100

    Login design changes 1

--- app/src/main/java/home/amml/ad/second_pc_store/view/fragment/FirstFragment.java
+++ app/src/main/java/home/amml/ad/second_pc_store/view/fragment/FirstFragment.java
@@ -30,7 +30,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.btLoginFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
